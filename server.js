e7b582f33ae2bce859d4430d9fd47a1987ca6007
zcollin <40807332+zcollin@users.noreply.github.com>
2020-11-08 19:35:50 -0500

    Package/Server Update

--- server.js
+++ server.js
@@ -2,6 +2,11 @@
 /* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
 import express from 'express';
+import dotenv from 'dotenv';
+import fetch from 'node-fetch';
+import countries from './public/lab_6/countries.js';
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,12 +15,23 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.route('/api')
   .get((req, res) => {
     console.log('GET request detected');
+    res.send(`Lab 5 for ${process.env.NAME}`);
   })
-  .post((req, res) => {
+  .post(async(req, res) => {
     console.log('POST request detected');
+    const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+    const json = await data.json();
+    console.log('Fetch request data', data);
+    res.json(json);
   });
 
 app.listen(port, () => {
