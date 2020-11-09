0f2a250418cf23d4059b6069f45f4093fd4e269c
zcollin <40807332+zcollin@users.noreply.github.com>
2020-11-09 14:53:59 -0500

    Updated CSS and JS

--- public/script.js
+++ public/script.js
@@ -1,3 +1,4 @@
+//Server Call
 jsonList = [];
 
 fetch('/api', {
@@ -12,6 +13,8 @@ fetch('/api', {
     console.log(err);
   });
 
+  //Functions
+
 function findMatches(wordToMatch, restaurantList) {
     return restaurantList.filter(restaurant => {
         const regex = new RegExp(wordToMatch, 'gi');
@@ -19,17 +22,42 @@ function findMatches(wordToMatch, restaurantList) {
     });
 }
 
-  function runThisWithResultsFromServer(input, jsonFromServer) {
+  function runThisAfterEvent(input, jsonFromServer) {
     // console.log('jsonFromServer', jsonFromServer);
     sessionStorage.setItem('restaurantList', JSON.stringify(jsonFromServer)); // don't mess with this, we need it to provide unit testing support
     // Process your restaurants list
     const matchArray = findMatches(input, jsonFromServer);
-    console.log(matchArray);
+
+    ul = document.querySelector('ul')
+    ul.innerHTML = '';
+
+    matchArray.forEach((el, i) => {
+      let li = document.createElement('li');
+      let li2 = document.createElement('li2');
+      let li3 = document.createElement('li3');
+      let li4 = document.createElement('li4');
+
+
+      li.innerHTML = (`<span class="name">${el.name}</span><br>`);
+      li2.innerHTML = (`<span class="category">${el.category}</span><br>`);
+      li3.innerHTML = (`<span class="address">${el.address_line_1}<br>${el.city}</span><br>`);
+      li4.innerHTML = (`<span class="zip">${el.zip}\n</span><br>`);
+      li.append(li2);
+      li.append(li3);
+      li.append(li4);
+
+      ul.append(li);
+    });
   }
   
-  // Leave lines 52-67 alone; do your work in the functions above
   document.body.addEventListener('input', async (e) => {
     e.preventDefault(); // this stops whatever the browser wanted to do itself.
     input = document.querySelector('input').value;
-    runThisWithResultsFromServer(input,jsonList)
+    console.log(input);
+    if(input == '') {
+      ul = document.querySelector('ul')
+      ul.innerHTML = '';
+    } else {
+      runThisAfterEvent(input,jsonList)
+    }
   });
